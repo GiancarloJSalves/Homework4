53fab66a29aae8ed3e2a4fb6ee790fee5ab8745d
Giancarlo Alves <gjsavoryalves@gmail.com>
2026-02-16 21:14:07 +0000

    homework without packages files

--- models/marts/dim_zones.sql
+++ models/marts/dim_zones.sql
@@ -1,5 +1,12 @@
 with taxi_zone_lookup AS (
     SELECT * FROM {{ ref("taxi_zone_lookup")}}
-)
+),
 
-SELECT * FROM taxi_zone_lookup
+renamed AS (select
+    locationid as location_id,
+    borough,
+    zone,
+    service_zone
+from {{ ref('taxi_zone_lookup') }} )
+
+SELECT * FROM renamed 
